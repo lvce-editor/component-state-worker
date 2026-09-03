f2ddb1e53db7e308fa71c92c5879da45d604f323
Le Vivilet <levivilet@pm.me>
2026-09-03 20:22:50 +0000

    feature: enable stricter eslint presets (#10)

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,14 @@ import * as config from '@lvce-editor/eslint-config'
 
 export default defineConfig([
   ...config.default,
+  ...config.recommendedVirtualDom,
+  ...config.recommendedVirtualDomStrict,
+  ...config.recommendedRegex,
+  ...config.recommendedTsconfig,
+  ...config.recommendedActions,
+  ...config.recommendedNvmrc,
+  ...config.recommendedE2e,
+  ...config.recommendedRpc,
   {
     rules: {
       '@cspell/spellchecker': 'off',
