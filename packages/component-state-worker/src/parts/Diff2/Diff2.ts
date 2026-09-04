a2fddc99f52088e4a5f6167aa84c9a52fea1d6df
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-04 09:53:30 +0000

    feature: render component cards in flex rows

--- packages/component-state-worker/src/parts/Diff2/Diff2.ts
+++ packages/component-state-worker/src/parts/Diff2/Diff2.ts
@@ -1,6 +1,11 @@
 import * as ComponentStateViewStates from '../ComponentStateViewStates/ComponentStateViewStates.ts'
+import { getColumnCount } from '../GetColumnCount/GetColumnCount.ts'
 
 export const diff2 = (uid: number): readonly number[] => {
   const { oldState, scheduledState } = ComponentStateViewStates.get(uid)
-  return oldState.components === scheduledState.components && oldState.loaded === scheduledState.loaded ? [] : [1]
+  return oldState.components === scheduledState.components &&
+    oldState.loaded === scheduledState.loaded &&
+    getColumnCount(oldState.width) === getColumnCount(scheduledState.width)
+    ? []
+    : [1]
 }
