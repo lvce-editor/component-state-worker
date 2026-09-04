02dd2337a1a4cd52c01f7386b7dca80b3fe4dfae
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-04 09:20:43 +0000

    feature: link component state worker in dev

--- packages/build/src/dev.ts
+++ packages/build/src/dev.ts
@@ -1,12 +1,17 @@
 import { execa } from 'execa'
 import { root } from './root.ts'
 
+await execa('npm', ['run', 'build'], {
+  cwd: root,
+  stdio: 'inherit',
+})
+
 execa('npm', ['run', 'build:watch'], {
   cwd: root,
   stdio: 'inherit',
 })
 
-execa('node', ['node_modules/@lvce-editor/server/bin/server.js', '--test-path=packages/e2e'], {
+execa('node', ['node_modules/@lvce-editor/server/bin/server.js', '--link=.tmp/dist', '--test-path=packages/e2e'], {
   cwd: root,
   stdio: 'inherit',
 })
