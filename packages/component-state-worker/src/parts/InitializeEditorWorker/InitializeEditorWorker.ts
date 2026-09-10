76ede873ab667bb3b699b3bf2485fbdedf0b791b
Le Vivilet <levivilet@pm.me>
2026-09-10 20:45:04 +0200

    feature: simplify component state rpc dependencies (#51)

--- packages/component-state-worker/src/parts/InitializeEditorWorker/InitializeEditorWorker.ts
+++ packages/component-state-worker/src/parts/InitializeEditorWorker/InitializeEditorWorker.ts
@@ -1,11 +1,6 @@
-import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
-import { EditorWorker, RendererWorker } from '@lvce-editor/rpc-registry'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
 import * as EditorChangeListener from '../EditorChangeListener/EditorChangeListener.ts'
 
 export const initializeEditorWorker = async (): Promise<void> => {
-  const rpc = await LazyTransferMessagePortRpcParent.create({
-    commandMap: {},
-    send: (port: MessagePort) => RendererWorker.sendMessagePortToEditorWorker(port, EditorChangeListener.rpcId),
-  })
-  EditorWorker.set(rpc)
+  await RendererWorker.initializeEditorWorker(EditorChangeListener.rpcId)
 }
---
+++ packages/component-state-worker/test/InitializeRendererWorker.test.ts
@@ -0,0 +1,17 @@
+import { expect, jest, test } from '@jest/globals'
+
+jest.unstable_mockModule('@lvce-editor/rpc-registry', () => ({
+  EditorWorker: {},
+  RendererWorker: {
+    initializeRendererWorkerForWorker: jest.fn<(commandMap: object) => Promise<void>>().mockResolvedValue(undefined),
+  },
+}))
+
+const { RendererWorker } = await import('@lvce-editor/rpc-registry')
+const { commandMap } = await import('../src/parts/CommandMap/CommandMap.ts')
+const { initializeRendererWorker } = await import('../src/parts/InitializeRendererWorker/InitializeRendererWorker.ts')
+
+test('connects the renderer worker using the component state commands', async () => {
+  await initializeRendererWorker()
+  expect(RendererWorker.initializeRendererWorkerForWorker).toHaveBeenCalledWith(commandMap)
+})
