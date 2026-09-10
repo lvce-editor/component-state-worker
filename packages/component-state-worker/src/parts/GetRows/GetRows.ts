f982eb4c3c547b093b8cfc6a62f4222bd2eed206
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-10 23:14:11 +0000

    feature: simplify component state row rendering

--- packages/component-state-worker/src/parts/GetRows/GetRows.ts
+++ packages/component-state-worker/src/parts/GetRows/GetRows.ts
@@ -4,17 +4,16 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { getCard } from '../GetCard/GetCard.ts'
 
 export const getRows = (components: readonly ComponentInfo[], columnCount: number): readonly VirtualDomNode[] => {
-  const rows: VirtualDomNode[] = []
+  const rows: ComponentInfo[][] = []
   for (let index = 0; index < components.length; index += columnCount) {
-    const row = components.slice(index, index + columnCount)
-    rows.push(
-      {
-        childCount: row.length,
-        className: ClassNames.Row,
-        type: VirtualDomElements.Div,
-      },
-      ...row.flatMap(getCard),
-    )
+    rows.push(components.slice(index, index + columnCount))
   }
-  return rows
+  return rows.flatMap((row) => [
+    {
+      childCount: row.length,
+      className: ClassNames.Row,
+      type: VirtualDomElements.Div,
+    },
+    ...row.flatMap(getCard),
+  ])
 }
