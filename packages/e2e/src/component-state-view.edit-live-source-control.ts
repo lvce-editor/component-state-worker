c42dc4ce35936d0cec0ff604fe0d29d419d09691
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-15 11:02:46 +0000

    test: format source control activation setup

--- packages/e2e/src/component-state-view.edit-live-source-control.ts
+++ packages/e2e/src/component-state-view.edit-live-source-control.ts
@@ -2,7 +2,18 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'component-state-view.edit-live-source-control'
 
-export const test: Test = async ({ Command, ComponentState, Developer, Editor, expect, FileSystem, Locator, Settings, SideBar, Workspace }) => {
+export const test: Test = async ({
+  Command,
+  ComponentState,
+  Developer,
+  Editor,
+  expect,
+  FileSystem,
+  Locator,
+  Settings,
+  SideBar,
+  Workspace,
+}) => {
   await Command.execute('ExtensionManagement.activateByEvent', 'onLanguage:json')
   await Command.execute('Layout.handleExtensionsChanged')
   await Settings.update({ 'editor.fontFamily': 'monospace' })
